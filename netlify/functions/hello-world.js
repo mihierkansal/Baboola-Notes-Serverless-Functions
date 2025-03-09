4bc24d3897b62e0507b86decc071995fa0aef1b8
mihierkansal <mihierkansal@outlook.com>
2025-03-09 11:08:19 -0700

    Allow other domians

--- netlify/functions/hello-world.js
+++ netlify/functions/hello-world.js
@@ -1,6 +1,10 @@
 exports.handler = async (event, context) => {
   return {
     statusCode: 200,
+    headers: {
+      "Content-Type": "text/plain", // For plain text responses
+      "Access-Control-Allow-Origin": "*", // Allows requests from any origin
+    },
     body: '{ msg: "Hello, World!" }',
   };
 };
