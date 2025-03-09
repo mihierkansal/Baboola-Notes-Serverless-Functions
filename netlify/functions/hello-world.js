a873ee9296903b6a9a63571339d4de4722246b86
mihierkansal <mihierkansal@outlook.com>
2025-03-09 11:32:23 -0700

    add some EN V

--- netlify/functions/hello-world.js
+++ netlify/functions/hello-world.js
@@ -1,4 +1,5 @@
 const mongodb = require("mongodb");
+require("dotenv").config();
 exports.handler = async (event, context) => {
   console.log(mongodb);
   return {
@@ -7,6 +8,9 @@ exports.handler = async (event, context) => {
       "Content-Type": "text/plain", // For plain text responses
       "Access-Control-Allow-Origin": "*", // Allows requests from any origin
     },
-    body: '{ msg: "Hello, World!" }',
+    body:
+      '{ msg: "Hello, World!", CDATA: ' +
+      process.env.MONGO_CONNECTION_STR +
+      " }",
   };
 };
